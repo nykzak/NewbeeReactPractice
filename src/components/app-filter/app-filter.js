24b1eec317730a912ba9d825621167e8c5386f83
Sergey <nykzak1@gmail.com>
2022-10-11 13:45:08 +0300

    Final

---
+++ src/components/app-filter/app-filter.js
@@ -0,0 +1,34 @@
+
+import './app-filter.css';
+
+const AppFilter = (props) => {
+    const buttonsData = [
+        {name: 'all',  label: 'Все сотрудники'},
+        {name: 'rise', label: 'На повышение'},
+        {name: 'more', label: 'З/П больше 1000$'}
+    ];
+
+    const buttons = buttonsData.map(({name, label}) => {
+        const active = props.filter === name;
+        const clazz = active ? 'btn-light' : 'btn-outline-light'
+
+
+        return (
+            <button 
+            className={`btn ${clazz}`}
+            type="button"
+            ket={name}
+            onClick={() => props.onFilterSelect(name)}>
+            {label}
+            </button>
+        )
+    })
+
+    return (
+        <div className="btn-group">
+        {buttons}
+        </div>
+    );
+}
+
+export default AppFilter;
---
+++ src/components/employees-list/employees-list.js
@@ -0,0 +1,29 @@
+import './employees-list.css';
+import EmployeesListItem  from '../employees-list-item/employees-list-item';
+
+const EmployeesList = ({data, onDelete, onToggleProp}) => {
+
+    const elements = data.map(item => {
+        const {id, ...itemProps} = item;
+
+        return(
+            <EmployeesListItem 
+             key={id}
+             {...itemProps}
+            onDelete={() => onDelete(id)}
+            onToggleProp = {(e) => onToggleProp(id, e.currentTarget.getAttribute('data-toggle'))}
+            // onToggleRise = {() => onToggleRise(id)}
+             />
+        )
+    })
+
+    
+
+    return (
+        <ul className="app-list list-group">
+           {elements}
+        </ul>
+    )
+}
+
+export default EmployeesList;
